263001ff4ecfa032f45fea76a34e9b039e68afb2
ranuarga <august.arga@gmail.com>
2020-01-13 14:15:51 +0700

    Update: Who Am I

--- public/liff-starter.js
+++ public/liff-starter.js
@@ -135,7 +135,20 @@ function registerButtonHandlers() {
     document.getElementById('getProfileButton').addEventListener('click', function() {
         if (liff.isLoggedIn()) {
             liff.getProfile().then(function(profile) {
-                window.alert('Kamu ' + profile.displayName);
+                document.getElementById('userIdProfileField').textContent = profile.userId;
+                document.getElementById('displayNameField').textContent = profile.displayName;
+
+                // const profilePictureDiv = document.getElementById('profilePictureDiv');
+                // if (profilePictureDiv.firstElementChild) {
+                //     profilePictureDiv.removeChild(profilePictureDiv.firstElementChild);
+                // }
+                // const img = document.createElement('img');
+                // img.src = profile.pictureUrl;
+                // img.alt = 'Profile Picture';
+                profilePictureDiv.appendChild(img);
+
+                document.getElementById('statusMessageField').textContent = profile.statusMessage;
+                toggleProfileData();
             }).catch(function(error) {
                 window.alert('Error getting profile: ' + error);
             });
